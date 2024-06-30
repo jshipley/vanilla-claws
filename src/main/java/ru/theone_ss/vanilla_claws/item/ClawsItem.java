83e6af99d3e8880ff3ec64eb5b2465f41781ec31
Theoness1 <sasha.bolotskih2002@gmail.com>
2024-07-01 01:36:49 +0700

    Attack range changed to vanilla attribute

--- src/main/java/ru/theone_ss/vanilla_claws/item/ClawsItem.java
+++ src/main/java/ru/theone_ss/vanilla_claws/item/ClawsItem.java
@@ -1,31 +1,23 @@
 package ru.theone_ss.vanilla_claws.item;
 
-import com.google.common.collect.ImmutableMultimap;
 import net.minecraft.block.Blocks;
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.component.type.AttributeModifierSlot;
 import net.minecraft.component.type.AttributeModifiersComponent;
 import net.minecraft.component.type.ToolComponent;
-import net.minecraft.entity.attribute.EntityAttribute;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.item.*;
 import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Identifier;
 
 import java.util.List;
 
-public class ClawsItem extends SwordItem {
-    //private final float attackDamage;
+public class ClawsItem extends ToolItem {
 
     public ClawsItem(ToolMaterial toolMaterial, Settings settings) {
         super(toolMaterial, settings.component(DataComponentTypes.TOOL, createToolComponent()));
-        //this.attackDamage = damage + material.getAttackDamage();
-/*
-        ImmutableMultimap.Builder<EntityAttribute, EntityAttributeModifier> builder = ImmutableMultimap.builder();
-        builder.put(EntityAttributes.GENERIC_ATTACK_DAMAGE, new EntityAttributeModifier(BASE_ATTACK_DAMAGE_MODIFIER_ID, this.attackDamage, EntityAttributeModifier.Operation.ADD_VALUE));
-        builder.put(EntityAttributes.GENERIC_ATTACK_SPEED, new EntityAttributeModifier(BASE_ATTACK_SPEED_MODIFIER_ID, attackSpeed, EntityAttributeModifier.Operation.ADD_VALUE));
-        builder.put(EntityAttributes.PLAYER_ENTITY_INTERACTION_RANGE, new EntityAttributeModifier(-0.75, EntityAttributeModifier.Operation.ADD_VALUE));*/
     }
 
     private static ToolComponent createToolComponent() {
@@ -33,7 +25,11 @@ public class ClawsItem extends SwordItem {
                 List.of(
                         ToolComponent.Rule.ofAlwaysDropping(List.of(Blocks.COBWEB), 15.0F),
                         ToolComponent.Rule.of(BlockTags.LEAVES, 15.0F),
-                        ToolComponent.Rule.of(BlockTags.SWORD_EFFICIENT, 1.5F)), 1.0F, 2);
+                        ToolComponent.Rule.of(BlockTags.WOOL, 5.0F),
+                        ToolComponent.Rule.of(List.of(Blocks.VINE, Blocks.GLOW_LICHEN), 2.0F)
+                ),
+                1.0F,
+                1);
     }
 
     public static AttributeModifiersComponent createAttributeModifiers(int baseAttackDamage, float attackSpeed) {
@@ -50,11 +46,11 @@ public class ClawsItem extends SwordItem {
                         new EntityAttributeModifier(BASE_ATTACK_SPEED_MODIFIER_ID, attackSpeed, EntityAttributeModifier.Operation.ADD_VALUE),
                         AttributeModifierSlot.MAINHAND
                 )
-/*                .add(
+                .add(
                         EntityAttributes.PLAYER_ENTITY_INTERACTION_RANGE,
-                        new EntityAttributeModifier(EntityAttributes.PLAYER_ENTITY_INTERACTION_RANGE, interactRange, EntityAttributeModifier.Operation.ADD_VALUE),
+                        new EntityAttributeModifier(Identifier.of("player.entity_interaction_range"), -0.75, EntityAttributeModifier.Operation.ADD_VALUE),
                         AttributeModifierSlot.MAINHAND
-                )*/
+                )
                 .build();
     }
 
